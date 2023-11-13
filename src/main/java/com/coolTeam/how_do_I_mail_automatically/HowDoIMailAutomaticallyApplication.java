b4866989428dc22a16ea501d69b4f0ab5f2cbff0
GabiPascu <pop_gabi00@yahoo.com>
2023-11-13 18:24:45 +0200

    lets goo

---
+++ src/main/java/com/coolTeam/how_do_I_mail_automatically/HowDoIMailAutomaticallyApplication.java
@@ -0,0 +1,13 @@
+package com.coolTeam.how_do_I_mail_automatically;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowDoIMailAutomaticallyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowDoIMailAutomaticallyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coolTeam/how_do_I_mail_automatically/HowDoIMailAutomaticallyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coolTeam.how_do_I_mail_automatically;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowDoIMailAutomaticallyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
